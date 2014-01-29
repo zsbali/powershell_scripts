c8a40610775e1451b3ea3860514e51abbf1e3238
Balazs Zsely <zsely.balazs@hotmail.com>
2014-01-29 08:45:31 +0100

    update

--- create_filelist_csv.ps1
+++ create_filelist_csv.ps1
@@ -1,7 +1,7 @@
-$source_path = "d:\_move_to_Maxell_500Gb"
-$filelist_path= $source_path + "\filelist.csv"
+$source_dir = "d:"
+$filelist_path= $source_dir + "\filelist.csv"
 $files_to_include = "*.jpg", "*.jpeg", "*.avi", "*.mkv", "*.mpg", "*.mp[34]"
-$paths_to_exclude = " " # add one space if there is no path to exclude
+$dirs_to_exclude = " " # add one space if there is no path to exclude
 
 try {
 	rm $filelist_path
@@ -15,7 +15,7 @@ try {
 	exit
 }
 
-ls -LiteralPath $source_path -r -include $files_to_inlcude  | ? { 
-	$_.GetType().Name -eq "FileInfo" -and ! (Select-String -InputObject $_.fullname -Pattern $paths_to_exclude -SimpleMatch)  } |% { 
+ls $source_dir -r -include $files_to_include  | ? { 
+	$_.GetType().Name -eq "FileInfo" -and ! (Select-String -InputObject $_.fullname -Pattern $dirs_to_exclude -SimpleMatch)  } |% { 
       ($_.Name+ ","+ $_.Length + "," + $_.DirectoryName.Substring(2)) | Out-File -FilePath $filelist_path -Append -Encoding UTF8
 }
