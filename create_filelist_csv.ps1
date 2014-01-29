638cf6b4ad1cea9de66f278188d565ded9b11d6b
Balazs Zsely <zsely.balazs@hotmail.com>
2014-01-29 08:39:35 +0100

    update

--- create_filelist_csv.ps1
+++ create_filelist_csv.ps1
@@ -1,14 +1,21 @@
-$source_path = "d:\angol"
+$source_path = "d:\_move_to_Maxell_500Gb"
 $filelist_path= $source_path + "\filelist.csv"
-$files_to_filter = "*.jpg", "*.jpeg", "*.avi", "*.mkv", "*.mpg", "*.mp[34]"
-$paths_to_exclude = "i:\powershell_backup"
+$files_to_include = "*.jpg", "*.jpeg", "*.avi", "*.mkv", "*.mpg", "*.mp[34]"
+$paths_to_exclude = " " # add one space if there is no path to exclude
 
-rm $filelist_path
-"name,length,directory" | Out-File -FilePath $filelist_path -Append -Encoding UTF8
+try {
+	rm $filelist_path
+} catch [ ItemNotFoundException ] {
+}
 
-ls $source_path -r -include $files_to_filter  | ? { ! (Select-String -InputObject $_.fullname -Pattern $paths_to_exclude -SimpleMatch)  } |% { 
-   
-   #Write-Host $_.FullName
-   ($_.Name+ ","+ $_.Length + "," + $_.DirectoryName.Substring(2)) | Out-File -FilePath $filelist_path -Append -Encoding UTF8
-   
+try {
+	"name,length,path" | Out-File -FilePath $filelist_path -Append -Encoding UTF8
+} catch [ Exception ]
+{
+	exit
+}
+
+ls -LiteralPath $source_path -r -include $files_to_inlcude  | ? { 
+	$_.GetType().Name -eq "FileInfo" -and ! (Select-String -InputObject $_.fullname -Pattern $paths_to_exclude -SimpleMatch)  } |% { 
+      ($_.Name+ ","+ $_.Length + "," + $_.DirectoryName.Substring(2)) | Out-File -FilePath $filelist_path -Append -Encoding UTF8
 }
