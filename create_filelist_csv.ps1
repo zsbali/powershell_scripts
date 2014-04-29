cf7adcef47bcf8e6d6ad1f63fc0dab442c1f9ef0
zsbali <zsely.balazs@hotmail.com>
2014-04-29 10:25:06 +0200

    Update create_filelist_csv.ps1

--- create_filelist_csv.ps1
+++ create_filelist_csv.ps1
@@ -1,19 +1,23 @@
-$source_dir = "d:\"
-$filelist_path= $source_dir + "\filelist.csv"
-$files_to_include = "*.jpg", "*.jpeg", "*.avi", "*.mkv", "*.mpg", "*.mp[34]"
-$dirs_to_exclude = " " # add one space if there is no path to exclude
+$source_dir = "d:\1temp\tmp3"
+$outfile= $source_dir + "\filelist.csv"
+#[Array] $filepatterns_to_include = "*.jpg", "*.jpeg", "*.avi", "*.mkv", "*.mpg", "*.mp[34]" # pattern only, NO path
+[Array] $filepatterns_to_include = "*.*" 	# pattern only, NO path
+[Array] $filepatterns_to_exclude	 		# pattern only, NO path
+[Array] $files_to_exclude					# full path IS needed
 
-rm $filelist_path -ErrorAction SilentlyContinue
+rm $outfile -ErrorAction SilentlyContinue
 
 try {
-	"name,length,path" | Out-File -FilePath $filelist_path -Append -Encoding UTF8 -ErrorAction Stop
+	"name,length,path" | Out-File -FilePath $outfile -Append -Encoding UTF8 -ErrorAction Stop
 } catch [ Exception ]
 {
 	Write-Host "Can't write file, exiting."
 	exit
 }
 
-ls $source_dir -r -include $files_to_include  | ? { 
-	$_.GetType().Name -eq "FileInfo" -and ! (Select-String -InputObject $_.fullname -Pattern $dirs_to_exclude -SimpleMatch)  } |% { 
-      ($_.Name+ ","+ $_.Length + "," + $_.DirectoryName.Substring(2)) | Out-File -FilePath $filelist_path -Append -Encoding UTF8
+$files_to_exclude += $outfile
+
+ls $source_dir -r -include $filepatterns_to_include -Exclude $filepatterns_to_exclude | ? { 
+	$_.GetType().Name -eq "FileInfo" -and ! (Select-String -InputObject $_.fullname -Pattern $files_to_exclude -SimpleMatch)  } |% { 
+      ($_.Name+ ","+ $_.Length + "," + $_.DirectoryName.Substring(2)) | Out-File -FilePath $outfile -Append -Encoding UTF8
 }
