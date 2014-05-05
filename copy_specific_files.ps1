388db33b018472e20e12f05d3a592d32f265410b
zsbali <zsely.balazs@hotmail.com>
2014-05-05 11:43:22 +0200

    Update copy_specific_files.ps1

--- copy_specific_files.ps1
+++ copy_specific_files.ps1
@@ -1,9 +1,9 @@
 $source_path = "h:\"
 $destination_path = "d:\1temp\tmp4"
-$files_to_move = "*.mp3", "*.wma"
+$files_to_copy = "*.mp3", "*.wma"
 $paths_to_exclude = "i:\powershell_backup"
 
-ls $source_path -r -include $files_to_move  | ? { 
+ls $source_path -r -include $files_to_copy  | ? { 
    ! (Select-String -InputObject $_.fullname -Pattern $paths_to_exclude -SimpleMatch)  } | % { 
       $new_dir_of_files = join-path $destination_path $_.directory.FullName.SubString(3)
       if ( (test-path $new_dir_of_files) -eq 0)
