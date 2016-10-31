bae5b1780e87c8dbc51e79fbb581a9709a175569
zsbali <zsely.balazs@hotmail.com>
2016-10-31 10:07:33 +0100

    Update copy_specific_files.ps1
    -Force

--- copy_specific_files.ps1
+++ copy_specific_files.ps1
@@ -3,7 +3,7 @@ $destination_path = "d:\1temp\tmp4"
 $files_to_copy = "*.mp3", "*.wma"
 $paths_to_exclude = "i:\powershell_backup"
 
-ls $source_path -r -include $files_to_copy  | ? { 
+ls $source_path -Force -r -include $files_to_copy  | ? { 
    ! (Select-String -InputObject $_.fullname -Pattern $paths_to_exclude -SimpleMatch)  } | % { 
       $new_dir_of_files = join-path $destination_path $_.directory.FullName.SubString(3)
       if ( (test-path $new_dir_of_files) -eq 0)
